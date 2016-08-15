a7312bd81dc2b055e870441998121ca81fb3df28
Paul J Stevens <p.stevens@lukkien.com>
2016-08-15 17:39:20 +0200

    init

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='wagtail.wagtailtrans',
+    version='0.0.1',
+    description='A Django content management system focused on flexibility and user experience',
+    author='Matthew Westcott',
+    author_email='matthew.westcott@torchbox.com',
+    url='http://wagtail.io/',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
+    install_requires=['wagtail'],
+    namespace_packages=['wagtail'],
+)
