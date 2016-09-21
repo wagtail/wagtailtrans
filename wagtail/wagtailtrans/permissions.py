a26867d54d7b5a0664193fc7ac090404f3747c4d
Henk-Jan van Hasselaar <hj.vanhasselaar@lukkien.com>
2016-09-21 11:48:22 +0200

    Add group creation on language save

--- wagtail/wagtailtrans/permissions.py
+++ wagtail/wagtailtrans/permissions.py
@@ -2,6 +2,17 @@ from django.contrib.auth.models import Group
 from wagtail.wagtailcore.models import GroupPagePermission
 
 
+def get_or_create_language_group(language):
+    """Create new translator group for language
+
+    :param language: Language instance
+    :return: Group
+    """
+    group, created = Group.objects.get_or_create(
+        name='translator-%s' % language.code)
+    return group
+
+
 def create_group_page_permission(page, language):
     """Create new GroupPagePermissions for
     the newly created page
@@ -10,8 +21,7 @@ def create_group_page_permission(page, language):
     :param language: Language instance
     """
     translator_perms = ['edit', 'publish']
-    group, created = Group.objects.get_or_create(
-        name='translator-%s' % language.code)
+    group = get_or_create_language_group(language)
 
     for perm in translator_perms:
         GroupPagePermission.objects.create(
