e89720e9674f964550d05d69ecc4dff66876524b
Henk-Jan van Hasselaar <hj.vanhasselaar@lukkien.com>
2016-09-23 10:08:38 +0200

    Fix template dirs

--- wagtail/wagtailtrans/tests/settings/base.py
+++ wagtail/wagtailtrans/tests/settings/base.py
@@ -74,7 +74,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(PROJECT_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
