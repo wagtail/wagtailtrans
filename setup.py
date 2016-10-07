923eb4bee004cf16213a3dd65ca8226ddccaf90f
Mike Dingjan <m.dingjan@lukkien.com>
2016-10-07 10:36:03 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -24,13 +24,15 @@ setup(
     license='BSD',
     long_description=open('README.rst').read(),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Framework :: Django',
         'Framework :: Django :: 1.8',
@@ -39,7 +41,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Site Management',
     ],
     install_requires=[
-        'wagtail>=1.6'
+        'wagtail>=1.6',
     ],
     tests_require=tests_require,
     extras_require={
