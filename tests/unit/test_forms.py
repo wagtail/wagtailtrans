5970b23c7fcbd0acd6180f8ec07e1af5c5b44480
Henk-Jan van Hasselaar <hj.vanhasselaar@lukkien.com>
2016-10-10 10:40:48 +0200

    Add unitest for language form

---
+++ tests/unit/test_forms.py
@@ -0,0 +1,36 @@
+import pytest
+
+from django.test import override_settings
+
+from tests.factories import language, pages, sites
+from wagtailtrans.models import Language, TranslatablePage
+from wagtailtrans.views import translation
+from wagtailtrans.forms import LanguageForm
+
+@pytest.mark.django_db
+class TestLanguageForms(object):
+
+    @override_settings(WAGTAILTRANS_SYNC_TREE=True)
+    def test_post_first_language(self):
+        Language.objects.all().delete()
+        data = {
+            'code': 'en',
+            'is_default': True,
+            'position': 0,
+            'live': True
+        }
+        form = LanguageForm(data)
+        assert form.is_valid()
+
+    @override_settings(WAGTAILTRANS_SYNC_TREE=True)
+    def test_post_new_default_language(self):
+        Language.objects.all().delete()
+        data = {
+            'code': 'fr',
+            'is_default': True,
+            'position': 0,
+            'live': True
+        }
+        form = LanguageForm(data)
+        assert form.is_valid()
+        assert not form.cleaned_data['is_default']
