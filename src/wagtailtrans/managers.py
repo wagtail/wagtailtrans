f3806cac9473c945839dfd6584f63a4ff299d528
Mike Dingjan <m.dingjan@lukkien.com>
2016-10-12 15:23:27 +0200

    Add .live() method for the LanguageManager

--- src/wagtailtrans/managers.py
+++ src/wagtailtrans/managers.py
@@ -4,6 +4,10 @@ from django.db import models
 class LanguageManager(models.Manager):
     """Custom manager for the `Language` model."""
 
+    def live(self):
+        """Return all the live languages."""
+        return self.filter(live=True)
+
     def default(self):
         """Return the first choice of default languages."""
-        return self.filter(live=True, is_default=True).first()
+        return self.live().filter(is_default=True).first()
