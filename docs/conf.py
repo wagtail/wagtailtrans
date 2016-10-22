b06c442e067719f8235d5cb9abd359280d24d795
Henk-Jan van Hasselaar <hj.vanhasselaar@lukkien.com>
2016-10-22 19:51:42 +0200

    RTD theme, intro text

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,9 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_rtd_theme
+
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
@@ -125,7 +128,9 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -134,7 +139,7 @@ html_theme = 'alabaster'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
--- docs/index.rst
+++ docs/index.rst
@@ -6,6 +6,18 @@
 Welcome to wagtailtrans's documentation!
 ========================================
 
+Wagtailtrans is a package that can be used to facilitate multi language sites in Wagtail.
+We developed wagtailtrans with two implementations in mind.
+A synchronised tree method and a freeform tree method.
+
+Synchronised trees is a method used to keep the tree structure in all languages the same.
+Any changes in the tree of the default language will also automatically be done in the language tree(s).
+
+Freeform trees allows the customization of the language trees.
+Pages can still be copied with content into the preferred language but this is not
+automatic nor mandatory.
+moderators can decide how the page structure works for every language.
+
 Contents:
 
 .. toctree::
