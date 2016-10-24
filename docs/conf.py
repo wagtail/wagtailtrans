71af7933e97e2dfb3271fa9fbe969de51cf96eb1
Mike Dingjan <m.dingjan@lukkien.com>
2016-10-24 13:30:09 +0200

    Remove `shpinx.ext.githubpages`, is not available on rtd.org

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,6 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
