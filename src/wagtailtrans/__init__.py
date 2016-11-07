5960008460bdc7ba8728bbe1fc4947614da171de
Henk-Jan van Hasselaar <hj.vanhasselaar@lukkien.com>
2016-11-07 15:40:00 +0100

    Whitespacing

--- src/wagtailtrans/__init__.py
+++ src/wagtailtrans/__init__.py
@@ -2,8 +2,8 @@ from os import path
 
 from django.utils.version import get_version
 
-
 VERSION = (0, 1, 0, 'final', 0)
+
 __version__ = get_version(VERSION)
 
 default_app_config = 'wagtailtrans.config.WagtailTransConfig'
