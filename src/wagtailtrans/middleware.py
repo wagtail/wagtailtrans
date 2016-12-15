c012761e93858eb24c58bdf0619e3faf72706236
Mike Dingjan <m.dingjan@lukkien.com>
2016-12-15 14:55:39 +0100

    Create TranslationMiddleware for earlier language activation
    When hitting a 404 / error page, the language isn’t activated, this middleware ensures that the a language is activated, ideally from the URL path with fallback to default language / settings defined language.
    
    This middleware replaces django LocalMiddleware

--- src/wagtailtrans/middleware.py
+++ src/wagtailtrans/middleware.py
@@ -1,25 +1,36 @@
-import logging
+from django import VERSION as django_version
+from django.conf import settings
+from django.utils import translation
 
-import django
+from .models import Language, SiteLanguages
+from .utils.conf import get_wagtailtrans_setting
 
-from .models import SiteLanguages
-from .thread import set_site_languages
-
-if django.VERSION >= (1, 10):
+if django_version >= (1, 10):
     from django.utils.deprecation import MiddlewareMixin
 else:
     MiddlewareMixin = object
 
-logger = logging.getLogger(__name__)
 
-
-class SiteLanguagesMiddleware(MiddlewareMixin):
-    """Make sure the SiteSetting object is loaded in the thread."""
+class TranslationMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
-        if not request.site:
-            logger.error("No site configured for request.")
+        language_from_path = translation.get_language_from_path(request.path)
+        if language_from_path:
+            active_language = language_from_path
+        elif get_wagtailtrans_setting('LANGUAGES_PER_SITE') and request.site:
+            site_languages = SiteLanguages.for_site(request.site)
+            active_language = site_languages.default_language.code
+        else:
+            default_language = Language.objects.default()
+            if default_language:
+                active_language = default_language.code
+            else:
+                active_language = settings.LANGUAGE_CODE
 
-        site_languages = SiteLanguages.for_site(request.site)
-        if site_languages:
-            set_site_languages(site_languages)
+        translation.activate(active_language)
+        request.LANGUAGE_CODE = active_language
+
+    def process_response(self, request, response):
+        if 'Content-Language' not in response:
+            response['Content-Language'] = request.LANGUAGE_CODE
+        return response
--- tests/unit/test_middleware.py
+++ tests/unit/test_middleware.py
@@ -1,16 +1,42 @@
 import pytest
 
-from tests.factories.requests import RequestFactory
-from wagtailtrans.middleware import SiteLanguagesMiddleware
-from wagtailtrans.models import SiteLanguages
+from django.test import Client, override_settings
+
+from wagtailtrans.models import Language
+
+from tests.factories.sites import SiteLanguagesFactory
+from tests.factories.language import LanguageFactory
 
 
 @pytest.mark.django_db
-def test_middleware():
-    """Verify that the sitelanguages attribute is set by the middleware."""
-    middleware = SiteLanguagesMiddleware()
-    rf = RequestFactory()
-    request = rf.get('/')
-    assert getattr(request.site, 'sitelanguages', None) is None
-    middleware.process_request(request)
-    assert isinstance(request.site.sitelanguages, SiteLanguages)
+class TestTranslationMiddleware(object):
+
+    def test_from_path(self):
+        response = Client().get('http://localhost:8000/nl/random/page/')
+        assert response.wsgi_request.LANGUAGE_CODE == 'nl'
+        assert response['Content-Language'] == 'nl'
+
+    def test_default_language(self):
+        LanguageFactory(code='en', is_default=True, live=True)
+        LanguageFactory(code='fr', is_default=False, live=True)
+
+        response = Client().get('http://localhost:8000/home/')
+        assert response.wsgi_request.LANGUAGE_CODE == 'en'
+        assert response['Content-Language'] == 'en'
+
+    def test_site_language(self):
+        site_lang = SiteLanguagesFactory(default_language__code='fr')
+
+        with override_settings(WAGTAILTRANS_LANGUAGES_PER_SITE=True):
+            response = Client().get('http://localhost:8000/random/page/')
+
+        assert response.wsgi_request.site == site_lang.site
+        assert response.wsgi_request.LANGUAGE_CODE == 'fr'
+        assert response['Content-Language'] == 'fr'
+
+    def test_settings_fallback(self):
+        Language.objects.all().delete()
+        with override_settings(LANGUAGE_CODE='en-us'):
+            response = Client().get('http://localhost:8000/random/page/')
+
+        assert response.wsgi_request.LANGUAGE_CODE == 'en-us'
