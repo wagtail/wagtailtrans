da44b0f038af5ad822cf629c888c81d90e73c576
Mike Dingjan <m.dingjan@lukkien.com>
2016-12-16 16:16:07 +0100

    removed comment

--- tests/_sandbox/settings/base.py
+++ tests/_sandbox/settings/base.py
@@ -69,7 +69,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
