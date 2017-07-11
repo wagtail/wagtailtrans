82e4eaebf58dc2c66e361565df678ca042df0fdd
arifin <arifin4web@gmail.com>
2017-07-11 12:01:54 +0600

    - Unittest for admin templatetags.

--- tests/unit/test_templatetags.py
+++ tests/unit/test_templatetags.py
@@ -1,8 +1,9 @@
 import pytest
 
-from wagtailtrans.templatetags import wagtailtrans_tags
+from wagtailtrans.templatetags import wagtailtrans_tags, translations_wagtail_admin
 
 from tests.factories.sites import create_site_tree, SiteFactory
+from tests.factories.pages import TranslatablePageFactory
 
 
 @pytest.mark.django_db
@@ -43,6 +44,22 @@ class TestWagtailtransTags(object):
         language_codes = [l.code for l in translations.keys()]
         assert language_codes[0] == 'en'
         assert language_codes[1] == 'es'
-        assert language_codes[2] == 'fr' 
+        assert language_codes[2] == 'fr'
         assert language_codes[3] == 'de'
         assert language_codes[4] == 'nl'
+
+
+@pytest.mark.django_db
+class TestTranslationWagtailAdminTags(object):
+
+    def test_get_canonical_pages_for_delete(self, languages):
+        site = SiteFactory()
+        page = TranslatablePageFactory.build(language=languages[0])
+        site.root_page.add_child(instance=page)
+        for lang in languages[1:]:
+            page.create_translation(lang)
+
+        canonical_pages = translations_wagtail_admin.get_canonical_pages_for_delete(page)
+
+        assert page not in canonical_pages
+        assert languages[1:].count() == canonical_pages.count()
