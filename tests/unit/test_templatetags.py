0c2ad74fbba780cebf392a11361ce3bafb8e2cfa
Md. Arifin Ibne Matin <arifin4web@gmail.com>
2017-07-12 17:46:58 +0600

    Unittest for admin templatetags (#96)
    * - Unittest for admin templatetags.
    
    * - include my name in contributors list :D
    
    * - Add test for cases when  function returns false and implement some other feedbacks.

--- tests/unit/test_templatetags.py
+++ tests/unit/test_templatetags.py
@@ -1,8 +1,10 @@
 import pytest
+from django.test import override_settings
 
-from wagtailtrans.templatetags import wagtailtrans_tags
-
-from tests.factories.sites import create_site_tree, SiteFactory
+from tests.factories.pages import TranslatablePageFactory, WagtailPageFactory
+from tests.factories.sites import SiteFactory, create_site_tree
+from wagtailtrans.templatetags import (
+    translations_wagtail_admin, wagtailtrans_tags)
 
 
 @pytest.mark.django_db
@@ -43,6 +45,23 @@ class TestWagtailtransTags(object):
         language_codes = [l.code for l in translations.keys()]
         assert language_codes[0] == 'en'
         assert language_codes[1] == 'es'
-        assert language_codes[2] == 'fr' 
+        assert language_codes[2] == 'fr'
         assert language_codes[3] == 'de'
         assert language_codes[4] == 'nl'
+
+
+@pytest.mark.django_db
+def test_get_canonical_pages_for_delete(languages):
+    page = create_site_tree(languages[0])[1]
+    for lang in languages[1:]:
+        page.create_translation(lang, copy_fields=True)
+
+    canonical_pages = translations_wagtail_admin.get_canonical_pages_for_delete(page)
+    assert page not in canonical_pages
+    assert languages[1:].count() == canonical_pages.count()
+
+    non_canocial_page = WagtailPageFactory(path='/root')
+    assert not translations_wagtail_admin.get_canonical_pages_for_delete(non_canocial_page)
+    
+    with override_settings(WAGTAILTRANS_SYNC_TREE=False):
+        assert not translations_wagtail_admin.get_canonical_pages_for_delete(page)
