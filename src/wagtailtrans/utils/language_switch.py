237c86fff0c7711969f6cbaac537984e588daa8e
Karl Hobley <karl@torchbox.com>
2019-02-22 16:25:41 +0000

    Update queries in language switch

--- src/wagtailtrans/utils/language_switch.py
+++ src/wagtailtrans/utils/language_switch.py
@@ -1,19 +1,18 @@
 from django.db import transaction
 from django.db.models import Q
 
-from wagtailtrans.models import Language, TranslatablePage
+from wagtailtrans.models import Language, get_translatable_models
 
 
 def create_new_canonical_page_mapping(new_language, queryset):
     return dict(queryset.filter(language_id=new_language.pk).values_list('canonical_page_id', 'pk'))
 
 
-def get_page_queryset(site=None):
+def get_page_queryset(model, site=None):
     if site is not None:
-        pk_list = site.root_page.get_descendants().values_list('pk', flat=True)
-        return TranslatablePage.objects.filter(pk__in=pk_list)
+        return model.objects.descendant_of(site.root_page)
 
-    return TranslatablePage.objects.all()
+    return model.objects.all()
 
 
 @transaction.atomic
@@ -31,15 +30,16 @@ def change_default_language(new_language, site=None):
         new_language.is_default = True
         new_language.save()
 
-    queryset = get_page_queryset(site)
-    mapping = create_new_canonical_page_mapping(new_language, queryset)
+    for model in get_translatable_models():
+        queryset = get_page_queryset(model, site)
+        mapping = create_new_canonical_page_mapping(new_language, queryset)
 
-    # New canonical languages should not have a canonical page
-    queryset.filter(pk__in=mapping.values()).update(canonical_page_id=None)
+        # New canonical languages should not have a canonical page
+        queryset.filter(pk__in=mapping.values()).update(canonical_page_id=None)
 
-    for old_pk, new_pk in mapping.items():
-        # Set new canonical page id to the new canonical pages.
-        queryset.filter(Q(canonical_page_id=old_pk) | Q(pk=old_pk)).update(canonical_page_id=new_pk)
-        # new canonical pages can't have a canonical page
-        # so we set the canonical_page_id to None
-        queryset.filter(pk=new_pk).update(canonical_page_id=None)
+        for old_pk, new_pk in mapping.items():
+            # Set new canonical page id to the new canonical pages.
+            queryset.filter(Q(canonical_page_id=old_pk) | Q(pk=old_pk)).update(canonical_page_id=new_pk)
+            # new canonical pages can't have a canonical page
+            # so we set the canonical_page_id to None
+            queryset.filter(pk=new_pk).update(canonical_page_id=None)
