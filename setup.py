c7ffe1cba6a98a78601ae73855c583a76bac6e6c
Janneke Janssen <j.janssen@lukkien.com>
2020-08-25 15:25:36 +0200

    Add django 3.1 and wt 2 classifier

--- setup.py
+++ setup.py
@@ -69,7 +69,9 @@ setup(
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Framework :: Wagtail',
+        'Framework :: Wagtail :: 2',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
     ]
 )
